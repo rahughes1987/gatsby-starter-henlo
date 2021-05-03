9a002a0c11c258651842347494694c955632a433
Rob Hughes <rob@whitemouse.com>
2021-05-03 13:47:23 +0100

    changing backend

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,7 +9,11 @@ CMS.init({
     load_config_file: false,
     display_url: 'http://localhost:8000',
     backend: {
-      name: 'git-gateway',
+      // name: 'git-gateway',
+      repo: 'https://github.com/rahughes1987/whitemouse',
+      api_root: 'https://api.github.com',
+      auth_endpoint: 'auth',
+      cms_label_prefix: 'netlify-cms/',
       branch: 'master',
     },
     media_folder: '/static/img',
